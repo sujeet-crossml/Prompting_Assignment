f1b3d59848f7f526169eabb17ebffc8ad0eddbcc
sujeet-crossml <sujeet.shah@crossml.com>
2025-12-30 14:30:42 +0530

    change quality of chode

--- client.py
+++ client.py
@@ -4,4 +4,4 @@ from cred import gemini_api_key
 
 
 # Creating client with gemini
-client = genai.Client(api_key=gemini_api_key)
+client = genai.Client(api_key = gemini_api_key)
--- cred.py
+++ cred.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 # Loading Gemini API key
 load_dotenv()
-gemini_api_key = os.getenv("GEMINI_API_KEY" ,default="")
+gemini_api_key = os.getenv("GEMINI_API_KEY", "")
 
 if not gemini_api_key: 
     raise EnvironmentError("Google_gemini_key is not found.")
